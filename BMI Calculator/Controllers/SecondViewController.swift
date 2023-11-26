2ad0f12495d41477a46cf2dfe69e102da6d507f2
Christopher Mena <you@chrismena.dev>
2023-11-25 22:56:12 -0500

    Created VC programatically

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Christopher Mena on 11/25/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
